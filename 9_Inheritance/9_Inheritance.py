7bbb9b3f8b950126700c64cc7a389c423dd2f628
Wilden Ali <wildeeeeen@gmail.com>
2018-07-22 11:22:18 +0700

    Create 9_Inheritance.py

---
+++ 9_Inheritance/9_Inheritance.py
@@ -0,0 +1,23 @@
+"""
+Apa itu Inheritance???
+PEWARISAN, you know lah pewarisan maksudnya Apa
+"""
+
+class Hero:
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
+
+class Hero_intelligent(Hero):
+    pass
+
+class Hero_strength(Hero):
+    pass
+
+lina = Hero('lina', 100)
+techies = Hero_intelligent('techies', 50)
+axe = Hero_strength('axe', 200)
+
+print(lina.name)
+print(techies.name)
+print(axe.name)
