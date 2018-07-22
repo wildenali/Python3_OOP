f032b5983c867db88dff6b639d850514c6994172
Wilden Ali <wildeeeeen@gmail.com>
2018-07-22 13:27:05 +0700

    Create 2a_constructor.py

---
+++ 2_Constructor/2a_constructor.py
@@ -0,0 +1,10 @@
+# Constructor adalah method yang akan dipanggil pertama kali secara otomatis
+# perbedaannya sama method biasa seperti class adalah dia harus di panggil manual
+
+# Constructor yang digunakan adalah __init__
+class Gorengan:
+    def __init__(self):
+        print("cireng")
+
+# panggil class Gorengan
+gorengan = Gorengan()
