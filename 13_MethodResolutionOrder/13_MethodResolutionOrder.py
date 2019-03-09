5ab332163be73b419dbc37676adb73a305ad1c16
wildenali <wildeeeeen@gmail.com>
2019-03-09 23:03:52 +0700

    cara mengetahui URUTAN PERINTAH

--- 13_MethodResolutionOrder/13_MethodResolutionOrder.py
+++ 13_MethodResolutionOrder/13_MethodResolutionOrder.py
@@ -14,5 +14,40 @@ class C(B,A):
 
 objek = C()
 objek.show()
-# cara melihat urutan eksekusinya adalah
+
+"""ini outputnya adalah
+ini adalah show B
+"""
+# terus kenapa ini adalah show A nya ga KELUAR coba?
+# cara cek nya adalah
+"""
+cara melihat urutan eksekusinya adalah
 help(objek)
+"""
+
+help(objek)
+
+""" bakal keluar informasi seperti berikut, jadi ini urutan programnya
+Help on C in module __main__ object:
+
+class C(B, A)
+ |  Method resolution order:
+ |      C
+ |      B
+ |      A
+ |      builtins.object
+ |
+ |  Methods inherited from B:
+ |
+ |  show(self)
+ |
+ |  ----------------------------------------------------------------------
+ |  Data descriptors inherited from B:
+ |
+ |  __dict__
+ |      dictionary for instance variables (if defined)
+ |
+ |  __weakref__
+ |      list of weak references to the object (if defined)
+(END)
+"""
