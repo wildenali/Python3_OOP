12537f0c36a7bdf8a947269d78c4acd26f55e420
wildenali <wildeeeeen@gmail.com>
2019-03-09 22:16:00 +0700

    decorator yg namanya @property  ,, merubah sebuah method menjadi seperti variable, ada juga @xxx.getter , @xxx.setter , @xxx.deleter

--- 08_GetterSetterDeleter/8_GetterSetterDeleter.py
+++ 08_GetterSetterDeleter/8_GetterSetterDeleter.py
@@ -1,3 +1,7 @@
+# decorator yg namanya @property  ,, merubah sebuah method menjadi seperti variable
+
+# ini cara encapsulation dengan menggunakan @property
+
 class Hero:
 
     def __init__(self, name, health, armor):
@@ -5,7 +9,10 @@ class Hero:
         self.__health = health
         self.__armor = armor
 
-    @property
+        # awalnya ini
+        # self.info = "name {} : \n\thealth: {}".format(self.name, self.__health)
+
+    @property   # decorator yg namanya @property  ,, merubah sebuah method menjadi seperti variable
     def info(self):
         return "name {} : \n\thealth: {}".format(self.name, self.__health)
 
@@ -13,15 +20,15 @@ class Hero:
     def armor(self):
         pass
 
-    @armor.getter
+    @armor.getter   # ini untuk menampilkan aja
     def armor(self):
         return self.__armor
 
-    @armor.setter
+    @armor.setter   # ini untuk mengubah
     def armor(self, input):
         self.__armor = input
 
-    @armor.deleter
+    @armor.deleter  # ini untuk menghapus aja
     def armor(self):
         print('armor di delet')
         self.__armor = None
@@ -44,3 +51,21 @@ print('delete armor')
 del sniper.armor
 print(sniper.armor)
 print(sniper.__dict__)
+
+
+""" outputnya
+merubah info
+name sniper :
+	health: 100
+name dadang :
+	health: 100
+getter dan setter untuk __armor
+10
+{'_Hero__health': 100, '_Hero__armor': 10, 'name': 'dadang'}
+50
+{'_Hero__health': 100, '_Hero__armor': 50, 'name': 'dadang'}
+delete armor
+armor di delet
+None
+{'_Hero__health': 100, '_Hero__armor': None, 'name': 'dadang'}
+"""
